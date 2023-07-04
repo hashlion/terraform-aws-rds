a750db01191c6ef65fa5c08971f16e50afa4e930
Lion Chen <lion.chen@hashicorp.com>
2023-07-04 16:34:54 -0600

    Updated files

--- variables.tf
+++ variables.tf
@@ -10,3 +10,24 @@ variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
 }
+
+variable "is_production" {
+  description = "Flag indicating if it's a production environment"
+  type        = bool
+}
+
+variable "max_connections" {
+  description = "Maximum allowed database connections"
+  type        = number
+  default     = 100
+}
+
+variable "allowed_cidrs" {
+  description = "List of allowed CIDR blocks"
+  type        = list(string)
+}
+
+variable "max_connections_no_defaults" {
+  description = "no default number"
+  type        = number
+}
